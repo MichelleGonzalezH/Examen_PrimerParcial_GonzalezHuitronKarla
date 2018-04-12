21e1d3ba53c8f2efe1d059bce313811756c90359
Karla Gonzalez <mitch_chenzon@hotmail.com>
2018-04-12 09:05:48 -0500

    Commit Inicial

--- app/src/main/java/mx/cecyt9/ipn/examen_primerparcial_gonzalezhuitronkarla/MainActivity.java
+++ app/src/main/java/mx/cecyt9/ipn/examen_primerparcial_gonzalezhuitronkarla/MainActivity.java
@@ -2,14 +2,36 @@ package mx.cecyt9.ipn.examen_primerparcial_gonzalezhuitronkarla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String Usuario = "Michelle";
+    String Password = "1234";
+    String UsuarioObtenido = "";
+    String PasswordObtenida = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+    }
+
+    public void validar(View v){
+        EditText tv = findViewById(R.id.usuario);
+        EditText tv2 = findViewById(R.id.password);
+        UsuarioObtenido = tv.getText().toString();
+        PasswordObtenida = tv2.getText().toString();
+        if(Usuario.equals(UsuarioObtenido)&&Password.equals(PasswordObtenida)){
+            //Mandar llamar actividad correcta
+            Toast.makeText(getApplicationContext(),"Usuario Correcto", Toast.LENGTH_LONG).show();
+        }
+        else{
+            //Mandar llamar actividad error
+            Toast.makeText(getApplicationContext(),"Usuario Incorrecto", Toast.LENGTH_LONG).show();
+        }
     }
 }
