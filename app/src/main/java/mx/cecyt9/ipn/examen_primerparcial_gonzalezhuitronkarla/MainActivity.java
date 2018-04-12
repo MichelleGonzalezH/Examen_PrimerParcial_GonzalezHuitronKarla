e1983e229947ce0d7725f959568b114b32edaa79
Karla Gonzalez <mitch_chenzon@hotmail.com>
2018-04-12 08:08:46 -0500

    Commit Inicial

---
+++ app/src/main/java/mx/cecyt9/ipn/examen_primerparcial_gonzalezhuitronkarla/MainActivity.java
@@ -0,0 +1,15 @@
+package mx.cecyt9.ipn.examen_primerparcial_gonzalezhuitronkarla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
