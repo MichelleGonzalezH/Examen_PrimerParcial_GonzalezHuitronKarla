a1897a9d07b19efe657df3910fb14a34b7185061
Karla Gonzalez <mitch_chenzon@hotmail.com>
2018-04-12 20:39:58 -0500

    Commit Tercero

--- app/src/main/java/mx/cecyt9/ipn/examen_primerparcial_gonzalezhuitronkarla/Error.java
+++ app/src/main/java/mx/cecyt9/ipn/examen_primerparcial_gonzalezhuitronkarla/Error.java
@@ -1,7 +1,9 @@
 package mx.cecyt9.ipn.examen_primerparcial_gonzalezhuitronkarla;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Error extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Error extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.error);
+    }
+
+    public void volver(View v){
+        Intent intento = new Intent(this,MainActivity.class);
+        startActivity(intento);
     }
 }
